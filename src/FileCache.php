91c952158dd4d9e9939402a3afb578f2bfac18de
Andreas Prucha (Abexto/Helicon) <andreas.prucha@gmail.com>
2018-04-30 08:17:36 +0200

    saveMode for FileCache

--- src/FileCache.php
+++ src/FileCache.php
@@ -15,4 +15,37 @@ class FileCache extends \yii\caching\FileCache implements common\CacheInterface
 {
     use common\CacheTrait;
     use common\CacheDefaultOverrideTrait;
+
+    /**
+     *
+     * @var type ̍@var bool Use file_exists before reading the cache file
+     */
+    public $saveMode = true;
+    
+    protected function fileExists($key)
+    {
+        return file_exists($this->getCacheFile($key));
+    }
+    
+    /**
+     * @inheritDoc
+     */
+    public function exists($key)
+    {
+        if (!$this->saveMode || $this->fileExists($key)) {
+            return parent::exists($key);
+        } else {
+            return false;
+        }
+    }
+    
+    protected function getValue($key)
+    {
+        if (!$this->saveMode || $this->fileExists($key)) {
+            return parent::getValue($key);
+        } else {
+            return false;
+        }
+    }
+    
 }
